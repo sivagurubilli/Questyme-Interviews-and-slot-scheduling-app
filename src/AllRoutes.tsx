588cd53ff050fc02c62bc53f5081408548dafb7d
praveen021097 <96250697+praveen021097@users.noreply.github.com>
2023-03-24 22:48:12 +0530

    user Dashboard layout

--- src/AllRoutes.tsx
+++ src/AllRoutes.tsx
@@ -4,6 +4,7 @@ import OneonOneEventsCreate from './Pages/AdminSidePages/AdminOneOnOneCreate'
 import OneonOneSlotsCreate from './Pages/AdminSidePages/OneOnOneSlotsEdit/OneOnOneSlotsCreate'
 import OneonOneEvents from './Pages/AdminSidePages/AdminOneOnOneInterviews'
 import Login from './Pages/Login/Login'
+import UserDashboard from './Components/UserDashboard/UserDashboard'
 
 const AllRoutes = () => {
   return (
@@ -14,6 +15,7 @@ const AllRoutes = () => {
             <Route path ="/admin/one-on-one-interviews/event-types" element={<OneonOneEvents/>}/>
             <Route path ="/admin/one-on-one-interviews/create" element={<OneonOneEventsCreate/>}/>
             <Route path ="/admin/one-on-one-interviews/edit/:id" element={<OneonOneSlotsCreate />}/>
+            <Route path='/user/me' element={<UserDashboard />} />
             </Routes>
     </div>
   )
--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -1,59 +1,58 @@
-
 import axios from "axios";
 import { IAuthlogin } from "./AuthInterface";
 
+export async function LoginService(data: IAuthlogin) {
+  const { username, password, rememberMe } = data;
 
-export async function LoginService(
-    data: IAuthlogin
-  ){
-    const { username, password,rememberMe } = data;
-  
-    try {
-      const response = await axios.post("/api/login", {
-       "username": username,
-        "password": password,
-      });
-     console.log(response)
-   if(response.data.token){
+  try {
+    const response = await axios.post("/api/login", {
+      username: username,
+      password: password,
+    });
+    console.log(response);
+    if (response.data.token) {
       //setting for remember me in
-      
-     if (rememberMe ) {
-      if(response.data.user.roles[0].name==="STUDENT_USER"){
-      localStorage.setItem("username", response.data.user.name);
-      localStorage.setItem("userId", response.data.user.id);
-      localStorage.setItem("userType",response.data.user.roles[0].name);
-      localStorage.setItem("token", response.data.token);
-      localStorage.setItem("batchId",response.data.user.batch.batchId)
-      localStorage.setItem("sectionId",response.data.user.section.sectionId)
-    }else{
-      localStorage.setItem("username", response.data.user.name);
-      localStorage.setItem("userId", response.data.user.id);
-      localStorage.setItem("userType",response.data.user.roles[0].name);
-      localStorage.setItem("token", response.data.token);
-    }
-  }
-    if (!rememberMe) {
-      if(response.data.user.roles[0].name==="STUDENT_USER"){
-      sessionStorage.setItem("username", response.data.user.name);
-      sessionStorage.setItem("userId", response.data.user.id);
-      sessionStorage.setItem("userType",response.data.user.roles[0].name)
-      sessionStorage.setItem("token", response.data.token);
-      sessionStorage.setItem("batchId",response.data.user.batch.batchId)
-      sessionStorage.setItem("sectionId",response.data.user.section.sectionId)
-    }else{
-      sessionStorage.setItem("username",response.data.user.name);
-      sessionStorage.setItem("userId", response.data.user.id);
-      sessionStorage.setItem("userType",response.data.user.roles[0].name)
-      sessionStorage.setItem("token", response.data.token);
-    }
-  }
-  }
-      return response.data;
-    } catch (error: any) {
-      console.log(error);
-      return error.response;
-    }
-  }
 
-
-  
+      if (rememberMe) {
+        if (response.data.user.roles[0].name === "STUDENT_USER") {
+          localStorage.setItem("username", response.data.user.name);
+          localStorage.setItem("userId", response.data.user.id);
+          localStorage.setItem("userType", response.data.user.roles[0].name);
+          localStorage.setItem("token", response.data.token);
+          localStorage.setItem("batchId", response.data.user.batch.batchId);
+          localStorage.setItem(
+            "sectionId",
+            response.data.user.section.sectionId
+          );
+        } else {
+          localStorage.setItem("username", response.data.user.name);
+          localStorage.setItem("userId", response.data.user.id);
+          localStorage.setItem("userType", response.data.user.roles[0].name);
+          localStorage.setItem("token", response.data.token);
+        }
+      }
+      if (!rememberMe) {
+        if (response.data.user.roles[0].name === "STUDENT_USER") {
+          sessionStorage.setItem("username", response.data.user.name);
+          sessionStorage.setItem("userId", response.data.user.id);
+          sessionStorage.setItem("userType", response.data.user.roles[0].name);
+          sessionStorage.setItem("token", response.data.token);
+          sessionStorage.setItem("batchId", response.data.user.batch.batchId);
+          sessionStorage.setItem(
+            "sectionId",
+            response.data.user.section.sectionId
+          );
+        } else {
+          sessionStorage.setItem("username", response.data.user.name);
+          sessionStorage.setItem("userId", response.data.user.id);
+          sessionStorage.setItem("userType", response.data.user.roles[0].name);
+          sessionStorage.setItem("token", response.data.token);
+        }
+      }
+    }
+    return response.data;
+  } catch (error: any) {
+    console.log(error);
+    return error.response;
+  }
+}
