742fc9708bc2e8224c7fc5c7a443c7e89235fb96
Shivamyadav07 <shivamvnsindia@gmail.com>
2023-03-29 19:24:14 +0530

    Update index.tsx

--- src/Pages/AdminSidePages/AdminBulkEventSchedule/AdminBulkEventCreate/index.tsx
+++ src/Pages/AdminSidePages/AdminBulkEventSchedule/AdminBulkEventCreate/index.tsx
@@ -9,8 +9,8 @@ const CreateBulkEvent = () => {
       <Navbar />
       <BulkEventNav />
       <Box w="80%" ml="10%" mt="60px" minH="200px" h="auto" p="5%" bg="white" borderRadius="10px" boxShadow="2px 4px 6px rgba(0, 0, 0, 0.1)">
-        <Box margin={"auto"} borderRadius={"10px"} boxShadow= "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" width={"50%"} p="20px">
-          <input style={{marginBottom:"20px", "fontWeight":"700"}} type="file" accept={".csv"} />
+        <Box margin={"auto"} borderRadius={"10px"} boxShadow="rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" width={"50%"} p="20px">
+          <input style={{ marginBottom: "20px", "fontWeight": "700" }} type="file" accept={".csv"} />
           <Button colorScheme="blue" >Schedule Interview</Button>
         </Box>
       </Box>
