99617beadfdc1c66a6199ce7dbbea37819c7d3a8
praveen021097 <96250697+praveen021097@users.noreply.github.com>
2023-04-08 17:01:28 +0530

    resolve conflicts login

--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -7,13 +7,8 @@ import { LoginData } from "../Pages/Login/LoginUser";
 
 export const loginService =(payload:LoginData)=>(dispatch:Dispatch<isLoginSuccess|isLoginFailure>):Promise<void | ActionTypes>=>{
 
-<<<<<<< Updated upstream
- return axios.post("https://fc11-2405-201-9009-9180-d5cf-7326-b382-f7da.in.ngrok.io/auth/login",payload).then((res)=>{
-  console.log("res.data",res.data);
-=======
  return axios.post("https://88ca-27-116-40-89.in.ngrok.io/auth/login",payload).then((res)=>{
   console.log("res.data");
->>>>>>> Stashed changes
   dispatch({type:ActionTypes.LOGIN_SUCCESS,payload:res.data})
   return ActionTypes.LOGIN_SUCCESS
  })
